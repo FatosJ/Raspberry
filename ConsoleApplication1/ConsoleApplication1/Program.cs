55c5d67f14dec008aad1a6f927a9519e4c4269ae
FatosJ <polis112>
2013-09-18 10:36:27 +0200

    Bilar

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Bilar
+{
+    public static void Main()
+    {
+        string bil1 = "BMW M5";
+        string bil2 = "Mercedes";   
+        string bil3 = "Ford";
+        string bil4 = "Ferrari";
+
+        int Häst1 = 500;
+        int Häst2 = 465;
+        int Häst3 = 550;
+        int Häst4 = 450;
+        
+        Console.WriteLine(bil1 + " har " + Häst1 + " hästkrafter" + ".");
+        Console.WriteLine(bil2 + " har " + Häst2 + " hästkrafter" + ".");
+        Console.WriteLine(bil3 + " har " + Häst3 + " hästkrafter" + ".");
+        Console.WriteLine(bil4 + " har " + Häst4 + " hästkrafter" + ".");
+    }  
+
+}
+
+
+        
+
